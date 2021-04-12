2725a55c58469ac5e322f993c0b000f53c46d3b1
Ciaran Kelly <Ciaran.Kelly@ergogroup.ie>
2021-04-12 16:56:14 +0100

    Created Coding Problems for .net interview

---
+++ Problem1Code.js
@@ -0,0 +1,13 @@
+let GivenArray = ['B','A','C','K','W','A','R','D','S']; 
+
+ 
+let ReverseArray = function(array){ 
+
+    // your code here.  
+
+}
+
+
+print(
+    //Print the answer to the problem 
+);
---
+++ Problem2Code.js
@@ -0,0 +1,16 @@
+
+
+
+var numberOfDays = function(startDate, endDate){
+    //Your code goes here
+    
+}
+
+
+var beginningDate = new Date(2020, 11, 24);
+var endDate = new Date(2021, 0, 7);
+
+print(
+   //Print the answer to the problem 
+);
+
